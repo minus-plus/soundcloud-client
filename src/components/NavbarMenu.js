64bbef7b0e7b1e7ced72b790015481993189fe00
Bruce <maljean2012@gmail.com>
2017-08-14 14:04:30 -0400

    nav bar search sign in

--- src/components/NavbarMenu.js
+++ src/components/NavbarMenu.js
@@ -3,6 +3,13 @@ import React, {Component} from 'react';
 class Navbar extends Component {
     constructor(props) {
         super(props);
+        this.handleKeyPress = this.handleKeyPress.bind(this);
+    }
+    
+    handleKeyPress(e){
+        if(e.key === 'Enter') {
+            console.log(e.target.value);
+        }
     }
 
     render() {
@@ -11,44 +18,22 @@ class Navbar extends Component {
             <div>
                 <div className="nav">
                     <div className="container clearfix">
-                        <div className="nav-logo">
-                            <button onClick={this.props.onAuth} type="button">Login</button>
-                        </div>
                         <div className="nav-nav float-left">
-                            {/*<div className="nav-nav-item">*/}
-                                {/*<a className="nav-nav-item-link active" href="/#/songs" title="">SoundRedux</a>*/}
-                            {/*</div>*/}
-                            {/*<div className="nav-nav-item">*/}
-                                {/*<a className="nav-nav-user-link " href="/#/me/stream" title="">*/}
-                                    {/*<span className="nav-nav-user-link-text">stream</span>*/}
-                                {/*</a>*/}
-                            {/*</div>*/}
-                            {/*<div className="nav-nav-item">*/}
-                                {/*<a className="nav-nav-user-link " href="/#/me/likes" title="">*/}
-                                    {/*<span className="nav-nav-user-link-text">likes</span></a>*/}
-                            {/*</div>*/}
-                            {/*<div className="nav-nav-item nav-playlists popover ">*/}
-                                {/*<div className="nav-nav-user-link ">*/}
-                                    {/*<span className="nav-nav-user-link-text">playlists</span>*/}
-                                    {/*<i className="icon ion-chevron-down"></i>*/}
-                                    {/*<i className="icon ion-chevron-up"></i>*/}
-                                {/*</div>*/}
-                            {/*</div>*/}
+                            <div className="nav-nav-item">
+                                <a className="nav-nav-item-link active" href="/" title="">Sound Client</a>
+                            </div>
                         </div>
                         <div className="nav-nav float-right">
-                            {/*<div className="nav-nav-item">*/}
-                                {/*<div className="nav-search">*/}
-                                    {/*<i className="icon ion-search">*/}
-                                    {/**/}
-                                    {/*</i>*/}
-                                    {/*<input type="text" className="nav-search-input" placeholder="SEARCH" />*/}
-                                {/*</div>*/}
-                            {/*</div>*/}
                             <div className="nav-nav-item">
-                                <div className="nav-user popover ">
-                                    <div className="nav-user-link">
-                                        <button onClick={this.props.onAuth} type="button">Login</button>
-                                    </div>
+                                <div className="nav-search">
+                                    <i className="fa fa-search" aria-hidden="true"> </i>
+                                    <input type="text" className="nav-search-input" placeholder="SEARCH" onKeyPress={this.handleKeyPress} />
+                                </div>
+                                
+                            </div>
+                            <div className="nav-nav-item">
+                                <div className="nav-logo">
+                                    <i className="fa fa-user fa-2x sign-in" onClick={this.props.onAuth} aria-hidden="true"></i>
                                 </div>
                             </div>
                         </div>
