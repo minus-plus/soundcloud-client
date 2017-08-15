11d5eb33682d76d0e0c783054d701dc8b5ea43d0
Bruce <maljean2012@gmail.com>
2017-08-15 14:07:24 -0400

    initial tracks

--- src/actions/trackActions.js
+++ src/actions/trackActions.js
@@ -2,9 +2,15 @@ const tracks = [
 
 ];
 
+
 export function getTracks() {
-    return {
-        type: "GET_TRACKS",
-        payload: tracks
+    return function(dispatch){
+        SC.get(`/tracks?q=house`, {limit:10, linked_partitioning: 1})
+            .then(function (res) {
+                dispatch({
+                    type:"GET_TRACKS",
+                    payload:res.collection
+                })
+            })
     }
 }
