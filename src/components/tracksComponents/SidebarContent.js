bbd813e937e26a18a8d25cb7a24082cedd961598
swli5 <swli5@hotmail.com>
2017-08-26 20:01:44 -0400

    comment box done, css almost done

--- src/components/tracksComponents/SidebarContent.js
+++ src/components/tracksComponents/SidebarContent.js
@@ -1,15 +1,36 @@
 import React from 'react';
+import moment from 'moment';
 
 export default function SidebarContent(props) {
-    const myComments = props.comments;
+    let myComments = props.comments;
     if (myComments.length === 0) {
         return <div>loading...</div>
     }
 
+    function compare(a,b) {
+        if (a.timestamp < b.timestamp)
+            return -1;
+        if (a.timestamp > b.timestamp)
+            return 1;
+        return 0;
+    }
+
+    myComments = myComments.sort(compare);
+
     const comment = myComments.map(comment => {
+        console.log(comment.timestamp);
+        if (comment.timestamp === null) {
+            comment.timestamp = 0;
+        }
+        let time = moment(comment.timestamp).format('mm:ss');
         return (
             <div className="comment" key={comment.id}>
-                {comment.body}
+                <div className="comment-image" style={{backgroundImage: `url(${comment.user.avatar_url})`}}></div>
+                <div className="comment-info">
+                    <div className="comment-comment">{comment.body}</div>
+                    <div className="comment-username">{comment.user.username}</div>
+                </div>
+                <div className="comment-time">{time}</div>
             </div>
         )
     });
