32a431de8d0b5ed000cc46ad1ebe1a568d5d0783
yun <wangyun732@gmail.com>
2017-09-08 00:31:15 -0400

    reset callback

--- src/constants/auth.js
+++ src/constants/auth.js
@@ -1,2 +1,2 @@
 export const CLIENT_ID = 'a281614d7f34dc30b665dfcaa3ed7505';
-export const REDIRECT_URI = `${window.location.protocol}//${window.location.host}/callback`;
+export const REDIRECT_URI = `https://soundcloud-client-redux.herokuapp.com/callback`;
